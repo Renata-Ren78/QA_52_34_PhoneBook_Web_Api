22847c1fa4e26d88ec77bf400363a20655e901a3
Renata-Ren78 <renatecertoka@yahoo.com>
2026-08-16 20:52:40 +0300

    DataProvider

---
+++ src/main/java/data_providers/UserDataProvider.java
@@ -0,0 +1,37 @@
+package data_providers;
+
+import dto.UserLombok;
+import org.testng.annotations.DataProvider;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class UserDataProvider {
+    @DataProvider
+    public Iterator<UserLombok> dataProviderWrongPasswordOrEmail(){
+        List<UserLombok> list = new ArrayList<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("src/test/resources/wrong_email - Sheet1.csv"))){
+            String line = bufferedReader.readLine();
+            while (line != null){
+                String[] splitLine = line.split(",");
+                list.add(UserLombok.builder()
+                        .username(splitLine[0])
+                        .password(splitLine[1])
+                        .build());
+                line = bufferedReader.readLine();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("created exception");
+        }
+        return list.listIterator();
+    }
+
+
+
+}
--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -1,5 +1,6 @@
 package ui_tests;
 
+import data_providers.UserDataProvider;
 import dto.UserLombok;
 import manager.AppManager;
 import org.testng.Assert;
@@ -53,6 +54,44 @@ public class RegistrationTests extends AppManager {
                 .contains("Wrong email or password format"));
 
     }
+
+    @Test
+    public  void registrationNegativeEmptyEmailFieldTest(){
+        UserLombok user = positiveUser();
+        user.setUsername("");
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+        Assert.assertTrue(loginPage.closeAlert()
+                .contains("Wrong email or password format"));
+    }
+
+
+    @Test
+    public  void registrationNegativeEmptyPasswordlFieldTest(){
+        UserLombok user = positiveUser();
+        user.setPassword("");
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+        Assert.assertTrue(loginPage.closeAlert()
+                .contains("Wrong email or password format"));
+    }
+
+    @Test(dataProvider = "dataProviderWrongPasswordOrEmail",
+            dataProviderClass = UserDataProvider.class)
+    public  void registrationNegativeWrongPasswordTest(UserLombok user){
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+        Assert.assertTrue(loginPage.closeAlert()
+                .contains("Wrong email or password format"));
+    }
+
+
+
+
+
+
+
+
 
 //    @Test
 //    public void testMethod(){
